4b2329b111f00933da1a1309858dec707fead8ce
Sravan voleti <sravankumar@codeaurora.org>
2021-08-24 22:45:02 +0530

    !fixup Bluetooth: Update Bluetooth permissions for internal APIS. (2/4)
    This adds attribution source as a parameter to Value added APIS.
    
    Update Bluetooth API annotations.
    
    CRs-Fixed: 3044065
    Change-Id: I64633662aa7bff72848dcf8d164bfbf1b5af2493
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -1170,7 +1170,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             }
             if (isBluetoothPersistedStateOnBluetooth() || !isBleAppPresent()) {
                 // This triggers transition to STATE_ON
-                mBluetooth.updateQuietModeStatus(mQuietEnable);
+                mBluetooth.updateQuietModeStatus(mQuietEnable,
+                        mContext.getAttributionSource());
                 mBluetooth.onLeServiceUp(mContext.getAttributionSource());
                 persistBluetoothSetting(BLUETOOTH_ON_BLUETOOTH);
             }
@@ -1988,7 +1989,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                                         Slog.i(TAG, "Already at BLE_ON State");
                                     } else {
                                         Slog.w(TAG, "BT Enable in BLE_ON State, going to ON");
-                                        mBluetooth.updateQuietModeStatus(mQuietEnable);
+                                        mBluetooth.updateQuietModeStatus(mQuietEnable,
+                                                mContext.getAttributionSource());
                                         mBluetooth.onLeServiceUp(mContext.getAttributionSource());
                                         persistBluetoothSetting(BLUETOOTH_ON_BLUETOOTH);
                                     }
