b914e541770b7e12bf8a4a9f58f2f713d7dcb076
johnmart19 <johnivan19999@gmail.com>
2022-03-30 21:36:55 +0300

    PixelPropsUtils: Do not change props for Fortnite * Else Game Crash after logo

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -133,8 +133,6 @@ public class PixelPropsUtils {
             "com.riotgames.league.wildrifttw",
             "com.riotgames.league.wildriftvn",
             "com.netease.lztgglobal",
-            "com.epicgames.fortnite",
-            "com.epicgames.portal"
     };
 
     private static ArrayList<String> allProps = new ArrayList<>(Arrays.asList("BRAND", "MANUFACTURER", "DEVICE", "PRODUCT", "MODEL", "FINGERPRINT"));
